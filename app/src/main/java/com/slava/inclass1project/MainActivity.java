0b307e05ad142efa63bf4f61e9e9f9262c8abcb1
SlavaPere <31996754+SlavaPere@users.noreply.github.com>
2019-09-17 09:34:52 -0400

    Initial commit

---
+++ app/src/main/java/com/slava/inclass1project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.slava.inclass1project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is my Toast! Quack!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
