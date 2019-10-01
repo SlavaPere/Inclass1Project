1db9e423b7e22d6ee389c9a176760b7d067f093a
SlavaPere <31996754+SlavaPere@users.noreply.github.com>
2019-10-01 09:55:15 -0400

    10/1/2019

--- app/src/main/java/com/slava/inclass1project/MainActivity.java
+++ app/src/main/java/com/slava/inclass1project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.slava.inclass1project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+/*
+        // splash activity
+        startActivity(new Intent(MainActivity.this,SplashActivity.class));
+        // close splash activity
+        finish();
+*/
         setContentView(R.layout.activity_main);
         mButton = findViewById(R.id.button);
         mButton.setOnClickListener(new View.OnClickListener() {
